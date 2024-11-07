370bbfb5e4adcb2c0ae37a33169806ff89f189e5
Electric & Tech World <lash.nkl@gmail.com>
2024-11-07 17:22:22 +0530

    first commit

--- routes/order.js
+++ routes/order.js
@@ -3,9 +3,9 @@ const { newOrder, getSingleOrder, myOrders, orders, updateOrder, deleteOrder } =
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 const router=express.Router();
 
-router.route('/order/new').post(isAuthenticatedUser, newOrder);
-router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
-router.route('/myorders').get(isAuthenticatedUser, myOrders);
+router.route('/order/new').post( newOrder);
+router.route('/order/:id').get( getSingleOrder);
+router.route('/myorders').get(myOrders);
 
 //Admin routes
 router.route('/admin/orders').get(isAuthenticatedUser, authorizeRoles('admin'),orders);
